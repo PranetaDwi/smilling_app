82110f354467d3959e9906c99f65b6ad80778d85
PranetaDwi <pranetadwiindarti2004@mail.ugm.ac.id>
2024-04-20 13:14:30 +0700

    feat:login view

--- app/src/main/java/com/example/smilling_app/views/SplashActivity.kt
+++ app/src/main/java/com/example/smilling_app/views/SplashActivity.kt
@@ -1,6 +1,9 @@
 package com.example.smilling_app.views
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +14,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        var handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(applicationContext, LoginActivity::class.java))
+            finish()
+        }, 3000L)
+
+        supportActionBar?.hide()
     }
 }
